25a5c7eacc6abe5466c345c1b54be652951ffad3
neverweep <xia.neverweep@gmail.com>
2013-07-12 19:39:51 +0800

    .

--- guestbook.php
+++ guestbook.php
@@ -12,18 +12,10 @@ Template Name: 留言板，只有评论会显示，如果你想和关于放到�
 	<div class="entry-header">
 		<h2 class="entry-title"><a href="<?php the_permalink();?>" title="<?php the_title();?>" rel="bookmark" itemprop="url"><?php the_title();?></a></h2>
 	</div>
-	<?php comments_template();?>
 </div>
-<?php if(current_user_can('edit_post')):?>
-<div class="entry-meta">
-	<div class="floatleft">
-		<?php edit_post_link('编辑','','');?>
-	</div>
-	<div class="clearfix"></div>
-</div>
-<?php endif;?>
-</div>
+<?php comments_template();?>
 <?php endwhile;?>
 <?php endif;?>
+</div>
 
 <?php get_footer();?>
